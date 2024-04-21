86191c2b191d8aa0293b807b44b150dbc87404df
jpfalcuci <jpfalcuci@gmail.com>
2024-04-21 10:55:04 -0300

    styles: Lilás

--- ToDoApp/src/components/TaskItem.js
+++ ToDoApp/src/components/TaskItem.js
@@ -8,7 +8,8 @@ const TaskItem = ({ item, navigation, toggleTaskCompletion, deleteTask }) => {
   return (
     <View style={styles.taskItem}>
       <Button
-        buttonStyle={{ backgroundColor: item.completed? Colors.taskCompleted : Colors.taskPending, marginRight: 10}}
+        buttonStyle={{ backgroundColor: Colors.buttonTertiary, marginRight: 10 }}
+        textStyle={{ color: item.completed? Colors.taskCompleted : Colors.taskFont}}
         content={ item.completed ? <Icon name='check-box' size={22}/> : <Icon name='check-box-outline-blank' size={22}/> }
         onPress={() => toggleTaskCompletion(item.id)}
       />
@@ -21,7 +22,8 @@ const TaskItem = ({ item, navigation, toggleTaskCompletion, deleteTask }) => {
         </Text>
       </TouchableOpacity>
       <Button
-        buttonStyle={{ backgroundColor: Colors.taskDelete, marginLeft: 10 }}
+        textStyle={{ color:Colors.taskDelete }}
+        buttonStyle={{ backgroundColor: Colors.buttonTertiary, marginLeft: 10 }}
         content={<Icon name='delete' size={22}/>}
         onPress={() => deleteTask(item.id)}
       />
@@ -37,7 +39,8 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 10,
     marginBottom: 10,
-    borderWidth: 1,
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
     borderColor: Colors.taskItemBorder,
     borderRadius: 5,
     backgroundColor: Colors.taskItemBackground,
